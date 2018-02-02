ceeb8050d9d89bcda9a91cb311268c78f49b6610
Steve Billings <sbillings@blackducksoftware.com>
2018-02-02 17:32:21 -0500

    Added a demo script

---
+++ src/main/resources/demo.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+docker pull blackducksoftware/hub-imageinspector-ws-alpine:0.1.0
+docker pull blackducksoftware/hub-imageinspector-ws-centos:0.1.0
+docker pull blackducksoftware/hub-imageinspector-ws-ubuntu:0.1.0
+
+mkdir /tmp/imageinspector
+docker pull alpine:latest
+docker save -o /tmp/imageinspector/alpine.tar alpine:latest
+
+docker pull fedora:latest # will run on centos
+docker save -o /tmp/imageinspector/fedora.tar fedora:latest
+
+docker pull debian:latest # will run on ubuntu
+docker save -o /tmp/imageinspector/debian.tar debian:latest
+
+echo Starting 3 imageinspector containers...
+docker run -v /tmp/imageinspector:/opt/blackduck/hub-imageinspector-ws/target -d --name hub-imageinspector-ws-alpine -p 8080:8080 blackducksoftware/hub-imageinspector-ws-alpine:0.1.0
+docker run -v /tmp/imageinspector:/opt/blackduck/hub-imageinspector-ws/target -d --name hub-imageinspector-ws-centos -p 8081:8081 blackducksoftware/hub-imageinspector-ws-centos:0.1.0
+docker run -v /tmp/imageinspector:/opt/blackduck/hub-imageinspector-ws/target -d --name hub-imageinspector-ws-ubuntu -p 8082:8082 blackducksoftware/hub-imageinspector-ws-ubuntu:0.1.0
+
+echo "Suggested tests:"
+echo curl -i http://localhost:8080/getbdio?tarfile=/opt/blackduck/hub-imageinspector-ws/target/alpine.tar
+echo curl -i http://localhost:8080/getbdio?tarfile=/opt/blackduck/hub-imageinspector-ws/target/fedora.tar
+echo curl -i http://localhost:8080/getbdio?tarfile=/opt/blackduck/hub-imageinspector-ws/target/debian.tar
+echo ""
+echo "The first request will get the result (BDIO) directly (because the package managers line up)."
+echo "For the 2nd and 3rd: Following the redirect will get you the result"
