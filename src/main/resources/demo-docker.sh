5f5d54cad4a320fd3327503408ac86933f4ff970
Steve Billings <sbillings@blackducksoftware.com>
2018-02-14 15:52:18 -0500

    0.2.0 with version fixed in demo scripts/config

--- src/main/resources/demo-docker.sh
+++ src/main/resources/demo-docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker pull blackducksoftware/hub-imageinspector-ws-alpine:0.1.0
-docker pull blackducksoftware/hub-imageinspector-ws-centos:0.1.0
-docker pull blackducksoftware/hub-imageinspector-ws-ubuntu:0.1.0
+docker pull blackducksoftware/hub-imageinspector-ws-alpine:0.2.0
+docker pull blackducksoftware/hub-imageinspector-ws-centos:0.2.0
+docker pull blackducksoftware/hub-imageinspector-ws-ubuntu:0.2.0
 
 mkdir /tmp/imageinspector
 docker pull alpine:latest
@@ -15,9 +15,9 @@ docker pull debian:latest # will run on ubuntu
 docker save -o /tmp/imageinspector/debian.tar debian:latest
 
 echo Starting 3 imageinspector containers...
-docker run -v /tmp/imageinspector:/opt/blackduck/hub-imageinspector-ws/target -d --name hub-imageinspector-ws-alpine -p 8080:8080 blackducksoftware/hub-imageinspector-ws-alpine:0.1.0
-docker run -v /tmp/imageinspector:/opt/blackduck/hub-imageinspector-ws/target -d --name hub-imageinspector-ws-centos -p 8081:8081 blackducksoftware/hub-imageinspector-ws-centos:0.1.0
-docker run -v /tmp/imageinspector:/opt/blackduck/hub-imageinspector-ws/target -d --name hub-imageinspector-ws-ubuntu -p 8082:8082 blackducksoftware/hub-imageinspector-ws-ubuntu:0.1.0
+docker run -v /tmp/imageinspector:/opt/blackduck/hub-imageinspector-ws/target -d --name hub-imageinspector-ws-alpine -p 8080:8080 blackducksoftware/hub-imageinspector-ws-alpine:0.2.0
+docker run -v /tmp/imageinspector:/opt/blackduck/hub-imageinspector-ws/target -d --name hub-imageinspector-ws-centos -p 8081:8081 blackducksoftware/hub-imageinspector-ws-centos:0.2.0
+docker run -v /tmp/imageinspector:/opt/blackduck/hub-imageinspector-ws/target -d --name hub-imageinspector-ws-ubuntu -p 8082:8082 blackducksoftware/hub-imageinspector-ws-ubuntu:0.2.0
 
 echo "Suggested tests:"
 echo curl -i http://localhost:8080/getbdio?tarfile=/opt/blackduck/hub-imageinspector-ws/target/alpine.tar
