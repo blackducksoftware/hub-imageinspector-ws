7735109d5562994b8e85c106e3b9f7648d1ca4b6
Steve Billings <sbillings@blackducksoftware.com>
2018-02-23 16:10:42 -0500

    refactoring

--- src/test/java/com/blackducksoftware/integration/hub/imageinspectorws/app/InMinikubeTest.java
+++ src/test/java/com/blackducksoftware/integration/hub/imageinspectorws/app/InMinikubeTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertTrue;
 import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
 
 import org.junit.AfterClass;
@@ -15,7 +14,6 @@ import org.junit.Test;
 import com.blackducksoftware.integration.exception.IntegrationException;
 import com.blackducksoftware.integration.hub.imageinspector.linux.executor.Executor;
 
-import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.PodList;
 import io.fabric8.kubernetes.api.model.Service;
@@ -23,7 +21,7 @@ import io.fabric8.kubernetes.api.model.ServiceList;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
 
-public class KubernetesViaFabric8ClientTest {
+public class InMinikubeTest {
     private static final String POD_NAME = "hub-imageinspector-ws";
     static KubernetesClient client;
 
@@ -41,8 +39,8 @@ public class KubernetesViaFabric8ClientTest {
             e.printStackTrace();
         }
 
-        final List<HasMetadata> resultPod = client.load(KubernetesViaFabric8ClientTest.class.getResourceAsStream("/kube-pod.yml")).inNamespace("default").createOrReplace();
-        final List<HasMetadata> resultSvc = client.load(KubernetesViaFabric8ClientTest.class.getResourceAsStream("/kube-service.yml")).inNamespace("default").createOrReplace();
+        client.load(InMinikubeTest.class.getResourceAsStream("/kube-pod.yml")).inNamespace("default").createOrReplace();
+        client.load(InMinikubeTest.class.getResourceAsStream("/kube-service.yml")).inNamespace("default").createOrReplace();
 
         final PodList podList = client.pods().inNamespace("default").list();
         for (final Pod pod : podList.getItems()) {
@@ -54,13 +52,13 @@ public class KubernetesViaFabric8ClientTest {
             System.out.printf("Service: %s; app: %s\n", service.getMetadata().getName(), service.getMetadata().getLabels().get("app"));
         }
         Thread.sleep(20000L);
-        final int healthCheckLimit = 10;
+        final int healthCheckLimit = 20;
         boolean serviceIsHealthy = false;
         for (int i = 0; i < healthCheckLimit; i++) {
             Thread.sleep(10000L);
             String[] healthCheckOutput;
             try {
-                System.out.printf("Health check attempt %d of %d:", i, healthCheckLimit);
+                System.out.printf("Health check attempt %d of %d:\n", i, healthCheckLimit);
                 healthCheckOutput = new Executor().executeCommand("curl -i http://192.168.99.100:8080/health", 5000L);
                 for (final String line : healthCheckOutput) {
                     System.out.printf("Health check output: %s\n", line);
@@ -73,8 +71,11 @@ public class KubernetesViaFabric8ClientTest {
                 if (serviceIsHealthy) {
                     break;
                 }
+                final String[] getPodsOutput = new Executor().executeCommand("kubectl get pods", 5000L);
+                final String getPodsOutputJoined = Arrays.asList(getPodsOutput).stream().collect(Collectors.joining("\n"));
+                System.out.printf("get pods output:\n%s\n", getPodsOutputJoined);
             } catch (final IntegrationException e) {
-                System.out.printf("Health check failed: %s", e.getMessage());
+                System.out.printf("Health check failed: %s\n", e.getMessage());
             }
         }
         assertTrue(serviceIsHealthy);
